4c1393f9af89e96f962ff949d454656fbb6c8e21
Dean Wilson <Dean Wilson>
2023-11-10 20:44:04 -0600

    Completed Orbit Report Assignment

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,15 +1,28 @@
+import './styling.css';
+
 const Table = ({ sat }) => {
   return (
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+        <th>Name</th>
+        <th>Type of Satellite</th>
+        <th>Launch Date</th>
+        <th>Status</th>
      </tr>
      </thead>
      <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
+      {sat.map((sat) => {
+        return(
+          <tr key={sat.id}>
+            <td>{sat.name}</td>
+            <td>{sat.type}</td>
+            <td>{sat.launchDate}</td>
+            <td>{sat.operational ? "active" : "inactive"}</td>
+          </tr>
+        )
+        }
+      )}
      </tbody>
    </table>
   );
